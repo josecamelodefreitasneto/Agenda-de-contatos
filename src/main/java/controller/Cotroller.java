ee93d5a280bf2e7c3c2bd1f7b98f87757bf70c6c
josecamelodefreitasneto <camelogamerdf@gmail.com>
2022-04-06 21:31:39 -0300

    Inclussão do arquivo style.css e JavaBeans.java

--- src/main/java/controller/Cotroller.java
+++ src/main/java/controller/Cotroller.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Cotroller
  */
-@WebServlet("/Cotroller")
+@WebServlet(urlPatterns ={"/Cotroller","/main"})
 public class Cotroller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- src/main/java/motel/JavaBeans.java
+++ src/main/java/motel/JavaBeans.java
@@ -1,5 +1,52 @@
 package motel;
 
 public class JavaBeans {
+	private String idicon;
+	private String nome;
+	private String fone;
+	private String email;
+	
+	
+	
+	public JavaBeans() {
+		super();
+		
+	}
+	
+	public JavaBeans(String idicon, String nome, String fone, String email) {
+		super();
+		this.idicon = idicon;
+		this.nome = nome;
+		this.fone = fone;
+		this.email = email;
+	}
+
+	public String getIdicon() {
+		return idicon;
+	}
+	public void setIdicon(String idicon) {
+		this.idicon = idicon;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFone() {
+		return fone;
+	}
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
 
 }
